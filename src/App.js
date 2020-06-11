f127c9085e98487157f7a08df17be70d3a140f91
ali <ali@iMac-de-Elodie.fritz.box>
2020-06-11 23:56:27 +0200

    fetures reducers

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import React from 'react';
-import {connect} from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
 
-import './App.css';
+import "./App.css";
 
+const App = props => {
+  console.log(props);
 
-const App=(props) => {
-  console.log(props)
- 
   return (
     <div className="App">
       <h1>Todo List</h1>
       <div className="ekleme_formu">
-        <input placeholder="Add to the list..."/>
+        <input placeholder="Add to the list..." />
         <button>Add</button>
       </div>
       <div className="liste">
-        {/* {liste.map(item => (
-          <div className={item.tamamlandi ? "yapildi" : ""}>{item.baslik}</div>
-        ))} */}
+        {props.liste.map(item => (
+          <div key={item.id} className={item.tamamlandi ? "yapildi" : ""}>
+            {item.baslik}
+          </div>
+        ))}
       </div>
       <button className="temizle">Clean up the dones</button>
     </div>
   );
-}
+};
 
 const mapStateToProps = state => {
   return {
-    liste:state.liste
-  }
-}
+    liste: state.liste
+  };
+};
 
-export default connect(mapStateToProps)(App); 
+export default connect(mapStateToProps)(App);
